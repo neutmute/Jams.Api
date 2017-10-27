30d8050ddde96e33d28d62eaf1b2e5ece476e7d9
neutmute <neutmute@users.noreply.github.com>
2017-10-27 11:34:46 +1100

    use .net core metadata

--- src/Jams.Api/Properties/AssemblyInfo.cs
+++ src/Jams.Api/Properties/AssemblyInfo.cs
@@ -1,20 +1,20 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Jams.Api")]
-[assembly: AssemblyDescription("A more sensible API for JAMS Scheduler")]
+//[assembly: AssemblyTitle("Jams.Api")]
+//[assembly: AssemblyDescription("A more sensible API for JAMS Scheduler")]
 
-[assembly: AssemblyCompany("neutmute")]
-[assembly: AssemblyProduct("Jams")]
-[assembly: ComVisible(false)]
+//[assembly: AssemblyCompany("neutmute")]
+//[assembly: AssemblyProduct("Jams")]
+//[assembly: ComVisible(false)]
 
 
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("Developer Build")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyInformationalVersion("Developer Build")]
 
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
+//#if DEBUG
+//[assembly: AssemblyConfiguration("Debug")]
+//#else
+//[assembly: AssemblyConfiguration("Release")]
+//#endif
